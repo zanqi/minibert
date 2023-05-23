8766f2ccf2ccaedf6521aaecfbf97f8eb88db68b
Zanqi Liang <zanqi.liang@gmail.com>
2023-05-22 20:09:44 -0700

    minibert implemented. Passed sanity check

---
+++ setup_mac.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+conda create -n cs224n_dfp python=3.8
+conda activate cs224n_dfp
+
+conda install pytorch torchvision torchaudio -c pytorch
+pip install tqdm
+pip install requests==2.25.1
+pip install importlib-metadata
+pip install filelock==3.0.12
+pip install sklearn==0.0
+pip install tokenizers==0.10.1
+pip install explainaboard_client==0.0.7
